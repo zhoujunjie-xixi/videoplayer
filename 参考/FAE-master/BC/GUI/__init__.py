4b86b39af0c7e0e38ce620913fef427f22d30f62
zhoujunjie-xixi <59299368+zhoujunjie-xixi@users.noreply.github.com>
2022-07-19 17:03:58 +0800

    Delete 参考 directory

--- 参考/FAE-master/BC/GUI/__init__.py
+++
@@ -1,4 +0,0 @@
-from BC.GUI.FeatureExtractionForm2 import FeatureExtractionForm
-from BC.GUI.PrepareForm import PrepareConnection
-from BC.GUI.ProcessForm import ProcessConnection
-from BC.GUI.VisualizationForm import VisualizationConnection
--- 参考/FAE-master/BC/Image2Feature/MergeQualityFeatures.py
+++
@@ -1,9 +0,0 @@
-# This class should be applied after Radiomics features extracted.
-from BC.Image2Feature.RadiomicsFeatureExtractor import RadiomicsFeatureExtractor
-
-class MergeQualityFeature():
-    def __init__(self, extractor):
-        self.extractor = extractor
-
-if __name__ == '__main__':
-    pass
